31a6ff07a8a40c5b235dc24e30e3026e49047ddd
Manish Kumar <man.kool2705@gmail.com>
2022-02-23 21:15:26 +0530

    adding 2.1 methods

---
+++ 2.1-Methods/src/method/overloading/MethodOverloading.java
@@ -0,0 +1,43 @@
+package method.overloading;
+//code depicting method overloading. which method to run is determined during compile time
+class TestOne{
+	int a,b;
+	void add(int a, int b, int c) {							// three arguments
+		System.out.println("method overloading(3 arguments): sum is  " + (a+b+c));
+	}
+	void add(int a, int b) {								//two  arguments
+		System.out.println("method overloading(2 arguments): sum is " + (a+b) );
+		
+	}
+	
+	int square(int a) {										//method to be overridden
+		this.a=a*a;
+		System.out.println("Testone method");
+		return this.a;
+	}
+	
+}
+
+class TestTwo extends TestOne{
+	int a=150;
+	 public int square(int a) {							//method overriding
+		this.a = a*a;
+		System.out.println("method overridding is presenting the sqaure of a value:");
+		return (this.a);								//this keyword referring to the instance of the class
+	}
+}
+
+public class MethodOverloading {
+public static void main(String[] args) {
+	TestOne obj1 = new TestOne();
+	obj1.add(5,5,5);									// calling add function with 3 arguments
+	obj1.add(6, 6);										// calling add function with 2 arguments.
+	
+	//-------------------->>objects for class testtwo
+	//TestTwo obj2 =  new  TestOne();  child class cant override parent;
+	TestOne obj2 = new TestTwo();		          //obj2 is reference  variable of type TestOne referring to the object of TestTwo; 
+	System.out.println(obj2.square(9));
+		
+}
+
+}
