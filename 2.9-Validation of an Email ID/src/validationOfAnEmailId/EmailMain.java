a009400080699a06d9d659d6f2089f74a282201f
Manish Kumar <man.kool2705@gmail.com>
2022-03-01 01:50:24 +0530

    added email validation over searching

--- 2.9-Validation of an Email ID/src/validationOfAnEmailId/EmailMain.java
+++ 2.9-Validation of an Email ID/src/validationOfAnEmailId/EmailMain.java
@@ -1,15 +1,16 @@
 package validationOfAnEmailId;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EmailMain {
 
 	public static void main(String[] args) {
-		 String[] email=new String[9];
-		 email[0]="manish.kumar@gmail.com";
+		 String[] email=new String[9];				//arr of strings
+		 email[0]="Manish.kumar2705@gmail.com";
 		 email[1]="ankit.kumar@gmail.com";
-		 email[2]="shewag@gmail.com";
+		 email[2]="shewag77@gmail.com";
 		 email[3]="dhoni@gmail.com"; 
 		 email[4]="virat@gmail.com"; 
 		 email[5]="ajay@gmail.com"; 
@@ -18,16 +19,23 @@ public class EmailMain {
 		 email[8]="rohit@gmail.com";
 
 		System.out.println("search database:\n");
+		//printing database
 		for(String i : email) {
 		System.out.println(i);
 		}
+		
+		//inputing user email for searching
 		System.out.println("\n");
 		 Scanner sc=new Scanner(System.in);
 		 System.out.println("enter the email id for searching");
 		 String inputEmail = sc.nextLine();
 		 
-		 
-		 
+		 //validation criteria
+		 Pattern p= Pattern.compile( "^[a-zA-Z0-9.]+@[a-z]+\\.[a-z]{2,3}");
+		Matcher m= p.matcher(inputEmail);
+		if(m.matches()==false) {System.out.println("invalid email entered");}
+		//if input matches the validation pattern then only check for match
+		if(m.matches()) {
 		int i=0;
 		 while( i < email.length) {
 			 boolean res =	Pattern.matches(email[i], inputEmail);
@@ -41,8 +49,8 @@ public class EmailMain {
 			 System.out.println("email match not found");
 		 	}
 		 sc.close();
-	 }
-		 
+	 	}
+	}
 		 
 		 
 }
