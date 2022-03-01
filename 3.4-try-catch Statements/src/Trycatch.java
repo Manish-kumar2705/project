884841e8b31eaf5db2f8ad4c0a03d1c3938a8bc1
Manish Kumar <man.kool2705@gmail.com>
2022-03-01 15:37:26 +0530

    3.4 initial

---
+++ 3.4-try-catch Statements/src/Trycatch.java
@@ -0,0 +1,16 @@
+
+
+public class Trycatch {
+	public static void main(String[] args) {
+		int a=1;
+		try {
+			a=5/0;
+			System.out.println(a);
+		
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("cant divide by zero");
+		}
+	}
+}
