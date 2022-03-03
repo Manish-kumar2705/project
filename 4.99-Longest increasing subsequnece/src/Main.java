af12c8aacf11170644a76f6f8d58bdd18fb9a72a
Manish Kumar <man.kool2705@gmail.com>
2022-03-03 21:59:20 +0530

    lic added comments

--- 4.99-Longest increasing subsequnece/src/Main.java
+++ 4.99-Longest increasing subsequnece/src/Main.java
@@ -6,17 +6,17 @@ public class Main {
 
 		int arr[] = { 15, 20, 68, 59, 35, 76, 97 , 38, 70, 92};
 		
-		int[] dp = new int[arr.length];
+		int[] dp = new int[arr.length];  //dp array to store length of the subsequence  of the current index.
 		
 		
-		int overallMax=0;
+		int overallMax=0;					//to store the overall max value of the dp array
 		for (int i=0; i< dp.length; i++) {
-			int max=0;
+			int max=0;						//local max to store the max for ith iteration 
 			
-			 for(int j = 0; j < i; j++ ) {
+			 for(int j = 0; j < i; j++ ) {	
 				 if(arr[j] < arr[i]) {
-					 if(dp[j] > max) {
-						 max = dp[j];
+					 if(dp[j] > max) {	
+						 max = dp[j];		
 					 }
 				 }
 			 }
@@ -25,7 +25,7 @@ public class Main {
 				 overallMax=dp[i];
 			 }
 		}
-		System.out.println(overallMax);
+		System.out.println("LIC is: " +overallMax);
 	}
 
 }
