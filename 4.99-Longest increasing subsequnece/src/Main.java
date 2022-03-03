1b02b3186be37acb521d2bcc23f41fda185904d2
Manish Kumar <man.kool2705@gmail.com>
2022-03-03 22:05:59 +0530

    lic added solution class

--- 4.99-Longest increasing subsequnece/src/Main.java
+++ 4.99-Longest increasing subsequnece/src/Main.java
@@ -6,26 +6,35 @@ public class Main {
 
 		int arr[] = { 15, 20, 68, 59, 35, 76, 97 , 38, 70, 92};
 		
+		Solution obj = new Solution();
+		obj.LIC(arr);
+	}
+}
+
+class Solution{
+	void LIC(int[] arr)
+	{
 		int[] dp = new int[arr.length];  //dp array to store length of the subsequence  of the current index.
-		
-		
+	
+	
 		int overallMax=0;					//to store the overall max value of the dp array
 		for (int i=0; i< dp.length; i++) {
 			int max=0;						//local max to store the max for ith iteration 
-			
-			 for(int j = 0; j < i; j++ ) {	
-				 if(arr[j] < arr[i]) {
+		
+			for(int j = 0; j < i; j++ ) {	
+				if(arr[j] < arr[i]) {
 					 if(dp[j] > max) {	
 						 max = dp[j];		
 					 }
-				 }
-			 }
-			 dp[i] = max + 1; 
-			 if(dp[i]>overallMax) {
-				 overallMax=dp[i];
-			 }
+				}
+			}
+			dp[i] = max + 1; 
+			if(dp[i]>overallMax) {
+				overallMax=dp[i];
+			}
 		}
 		System.out.println("LIC is: " +overallMax);
 	}
-
+		
 }
+	
